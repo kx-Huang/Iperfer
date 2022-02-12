03a6e3af6ceb90cfbe8315b66fd39420a348ba88
kx-huang <huang.kx@sjtu.edu.cn>
2022-02-12 00:51:17 -0600

    feat: test script and output

---
+++ script/assign1_test.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+"""
+Test topology for CS640, Spring 2022, Assignment 1
+"""
+
+from mininet.cli import CLI
+from mininet.net import Mininet
+from mininet.link import TCLink
+from mininet.topo import Topo
+from mininet.log import setLogLevel
+
+class AssignmentNetworks(Topo):
+    def __init__(self, **opts):
+        Topo.__init__(self, **opts)
+        h1 = self.addHost('h1')
+        h2 = self.addHost('h2')
+        h3 = self.addHost('h3')
+        h4 = self.addHost('h4')
+        s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
+        s3 = self.addSwitch('s3')
+        s4 = self.addSwitch('s4')
+        self.addLink(h1, s1)
+        self.addLink(h2, s2)
+        self.addLink(h3, s3)
+        self.addLink(h4, s4)
+        self.addLink(s1, s2, bw=20, delay='10ms')
+        self.addLink(s2, s3, bw=10, delay='15ms')
+        self.addLink(s3, s4, bw=20, delay='20ms')
+
+
+if __name__ == '__main__':
+    setLogLevel( 'info' )
+
+    # Create data network
+    topo = AssignmentNetworks()
+    net = Mininet(topo=topo, link=TCLink, autoSetMacs=True,
+           autoStaticArp=True)
+
+    # Run network
+    net.start()
+    CLI( net )
+    net.stop()
